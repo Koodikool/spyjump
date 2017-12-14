aef3a7a852a710bcb458afb996efc4d92e88aa14
henrykalju <henry.kaljulaid@gmail.com>
2017-12-14 19:59:54 +0200

    Lobby html fail

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res){
 	res.sendFile('views/index.html', {root: __dirname })
 });
 
+app.get('/lobby', function(req, res){
+	res.sendFile('views/lobby.html', {root: __dirname })
+})
+
 http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
