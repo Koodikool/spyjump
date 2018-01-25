ea9bf8f09933e26f048b5b03a0bb755c9d986539
Krister Viirsaar <krister.viirsaar@gmail.com>
2018-01-25 20:09:56 +0200

    Version 1.0

--- game.js
+++ game.js
@@ -11,30 +11,34 @@ var userStructure = {
 	admin: false
 }
 
+var placesDB = {
+	Koodikool: ['Häkker', 'Tainaaju', 'Külaline', 'Rassist', 'Koer', 'Õps'],
+	Kommipood: ['Mustas mantlis onu', 'Pervo', 'Väike laps', 'Välismaalane', 'Kassiir', 'Klient', 'Turvamees']
+}
+
 gameStart = function(socket) {
-	username = null
-	gameid = null
 
 	// User tells server their username and gameid
 	socket.on('register', function(data){
 		console.log("Register:", data)
+		console.log("User's socket ID:", socket.id, data.username)
+		var gameid = data.gameid.toLowerCase()
+		var isCreator = false
 
 		// Does gameid exist
-		if (database[data.gameid] === undefined) {
-			database[data.gameid] = gameStructure
-		}
+		if (database[gameid] === undefined) {
+			database[gameid] = gameStructure
+			isCreator = true
+		} 
 
-		// Does username exist
-		if (database[data.gameid].players[data.username] !== undefined) {
-			socket.emit('errorr', 'User already exists')
-			return
-		} else {
-			database[data.gameid].players[data.username] = userStructure
-			socket.emit('msg', 'User registered')
-		}
+		if (!database[gameid].players)
+			database[gameid].players = {}
+		database[gameid].players[socket.id] = Object.assign({}, userStructure)
+		database[gameid].players[socket.id].name = data.username
+		if (isCreator)
+			database[gameid].players[socket.id].admin = isCreator
 
-		username = data.username
-		gameid = data.gameid
+		socket.gameid = gameid
 		console.log("Database:", database)
 
 		socket.broadcast.emit('roomPlayerList', database)
@@ -42,19 +46,46 @@ gameStart = function(socket) {
 	})
 
 	socket.on('disconnect', function() {
-		console.log("User disconnected")
-		// totalConnections--
-		// socket.broadcast.emit('usercount', totalConnections)
-		console.log("User left game:", database[gameid])
-		if (!database[gameid]) {
+		console.log("User disconnected", socket.id)
+		if (!database[socket.gameid]) {
 			return
 		}
 
-		delete database[gameid].players[username]
+		delete database[socket.gameid].players[socket.id]
+		database = JSON.parse(JSON.stringify(database))
+		console.log("AFTER DELETE USER", database[socket.gameid])
 		socket.broadcast.emit('roomPlayerList', database)
-		if (Object.keys(database[gameid].players).length === 0) {
-			delete database[gameid]
+		if (Object.keys(database[socket.gameid].players).length === 0) {
+			console.log("DELETE GAME", socket.gameid)
+			delete database[socket.gameid]
 		}
+	})
+
+	socket.on('giveRoles', function() {
+		var randomPlaceNr = parseInt(Object.keys(placesDB).length * Math.random())
+		var place = Object.keys(placesDB)[randomPlaceNr]
+
+		if (!database[socket.gameid])
+			return
+
+		if (!database[socket.gameid].players[socket.id].admin)
+			return // if not admin, can't update game
+
+		var playersLength = Object.keys(database[socket.gameid].players).length
+		var spyNr = parseInt(playersLength * Math.random())
+
+		var index = 0
+		while (index < playersLength) {
+			var currentPlayer = Object.keys(database[socket.gameid].players)[index]
+			database[socket.gameid].players[currentPlayer].role = placesDB[place][index]
+			console.log(database[socket.gameid].players[socket.id].name)
+			index++
+		}
+		database[socket.gameid].place = place
+		database[socket.gameid].players[Object.keys(database[socket.gameid].players)[spyNr]].role = 'SPY'
+		socket.broadcast.emit('roomPlayerList', database)
+		socket.emit('roomPlayerList', database)
+		
 	})
 }
 
--- public/lobby.js
+++ public/lobby.js
@@ -1,9 +1,13 @@
 var socket = io()
 
-socket.emit('register', {
-	username: localStorage.getItem('username'),
-	gameid: localStorage.getItem('gameid')
-})
+setTimeout(function(){
+	socket.emit('register', {
+		username: localStorage.getItem('username'),
+		gameid: localStorage.getItem('gameid')
+	})
+},100)
+
+document.querySelector('#kood').innerHTML = localStorage.getItem('gameid')
 
 socket.on('usercount', function(data){
 	console.log("Usercount:", data)
@@ -17,18 +21,44 @@ socket.on('msg', function(data){
 	console.log("Message:", data)
 })
 
-socket.on('roomPlayerList', function(database){
-	var gameid = localStorage.getItem('gameid')
+socket.on('roomPlayerList', function(database, b, c){
+	var gameid = localStorage.getItem('gameid').toLowerCase()
 	var game = database[gameid]
+	if (!game) {
+		console.log("There's no such game")
+		return
+	}
 	var players = game.players
 
-	console.log(players)
+	if (database[gameid].players[socket.id]) {
+		var myRole = database[gameid].players[socket.id].role
+		var place = database[gameid].place
+		document.querySelector('#roll').innerHTML = 'Minu roll on ' + myRole
+		if (myRole !== 'SPY')
+			document.querySelector('#place').innerHTML = 'Koht on ' + place
+		else
+			document.querySelector('#place').innerHTML = ''
+	}
 
+	document.querySelector('.inimesed tbody').innerHTML = '' // Tühjenda tabel
 	Object.entries(players).forEach(function(keyvalue, obj){
-
+		var name = keyvalue[1].name
+		name = name.slice(0,20) // Et liiga pikk nimi ei saaks olla
+		name = name.replace('<', '')
 		document
 			.querySelector('.inimesed tbody')
-			.insertAdjacentHTML("beforeend", "<tr><td>"+keyvalue[0]+"</td></tr>")
-
+			.insertAdjacentHTML("beforeend", "<tr><td>"+name +"</td></tr>")
 	})
 })
+
+lahku = function() {
+	localStorage.setItem('gameid', null)
+	localStorage.setItem('username', null)
+	var foSure = confirm("Kindel?")
+	if (foSure)
+		window.location.href = '/'
+}
+
+giveRoles = function() {
+	socket.emit('giveRoles')
+}
